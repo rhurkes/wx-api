28398ef53adcb673236fe39aa8a1f1e65ed97f5e
Rob Hurkes <rob.hurkes@target.com>
2019-05-13 12:32:34 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ fn events_handler(state: State<AppState>, info: Path<u64>) -> Result<Json<Vec<Ev
     let start = wx::util::get_system_micros();
     let events_result = state.store_client.get_events(info.into_inner());
     let end = wx::util::get_system_micros();
-    println!("event_lookup: {}", end - start);
+    // println!("event_lookup: {}", end - start);
 
     match events_result {
         Ok(events) => Ok(Json(events)),
