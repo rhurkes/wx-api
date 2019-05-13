0c49c16c6d8b58b1a3b1200bf461046929976d58
Rob Hurkes <rob.hurkes@target.com>
2019-05-13 12:37:23 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -34,10 +34,10 @@ fn main() {
 }
 
 fn events_handler(state: State<AppState>, info: Path<u64>) -> Result<Json<Vec<Event>>, ApiError> {
-    let start = wx::util::get_system_micros();
+    // let start = wx::util::get_system_micros();
     let events_result = state.store_client.get_events(info.into_inner());
-    let end = wx::util::get_system_micros();
-    // println!("event_lookup: {}", end - start);
+    // let end = wx::util::get_system_micros();
+    println!("event_lookup: {}", "test");
 
     match events_result {
         Ok(events) => Ok(Json(events)),
